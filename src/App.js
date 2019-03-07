1ef8c37341f6650b3088f2f74c19b0838a53a261
Walden Wang <walwang@catorpww4003.global.publicisgroupe.net>
2019-03-07 13:34:58 -0500

    initial commit for todo list app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
+import xs from 'classnames';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+export default class TodoList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      item: {},
+      items: [],
+      doneItems: 0
+    };
+  }
+
+  onChange = (e) => {
+    const newItem = {
+      value: e.target.value,
+      done: false
+    };
+  
+    this.setState({ item: newItem });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.item.value) {
+      this.setState({
+        items: [...this.state.items, this.state.item],
+        item: {
+          value: ''
+        }
+      });  
+    }
+  }
+
+  toggleComplete = (item, index) => {
+    const newItems = []; //let
+ 
+    this.state.items.forEach((value, key) => {
+      if (key === index) {
+        value.done = !item.done;
+
+        this.doneItemsCounter(value.done);
+      }
+
+      newItems[key] = value;
+    })
+
+    this.setState({
+      items: newItems
+    })
+  }
+
+  doneItemsCounter = (increment) => {
+    if (increment) {
+      this.setState({
+        doneItems: this.state.doneItems + 1
+      })
+    } else {
+      this.setState({
+        doneItems: this.state.doneItems - 1
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div>
+          <h2>TodoList</h2>
+          <form onSubmit={this.onSubmit}>
+            <input value={this.state.item.value || ""} type="text" onChange={this.onChange} />
+            <button type="submit">Add</button> 
+          </form>
+          <p>{ this.state.items.length - this.state.doneItems } remaining out of { this.state.items.length } tasks</p>
+
+          <ul>
+            {
+              this.state.items.map((item, index) =>
+                <li
+                  key={index}
+                  onClick={() => this.toggleComplete(item, index)}
+                  className={ xs({'is-done': item.done})}>
+                  { item.value }
+                </li>
+              )
+            }
+          </ul>
+        </div>
+        <style>{`
+          .is-done {
+            text-decoration: line-through;
+          }
+        `}</style>
+      </>
     );
   }
 }
-
-export default App;
